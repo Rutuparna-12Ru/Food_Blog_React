0c22a0eae7731d4b00bb3f5885018710dffa736c
Rutuparna-12Ru <98957266+Rutuparna-12Ru@users.noreply.github.com>
2022-03-05 14:30:02 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,21 @@
+ import React from "react";
+
+
+ function About() {
+   return (
+     <div className="about">
+        <h1>Name</h1>
+           <button>Click Here </button>
+           <h2>Email</h2>
+           <button>Click Here</button>
+           <h3>Contact</h3>
+           <button>Click Here</button>
+       </div>
+       
+         
+     
+   );
+}
+
+export default About;
+
